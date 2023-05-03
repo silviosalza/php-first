41440862430bf213066fc7c345a5d1249ed6e084
silviosalza <silviofilipposalza@gmail.com>
2023-05-03 11:44:31 +0200

    push

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$username = "Silvio";
+
+
+echo "Ciao " . $username;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world from PHP</h1>
+    <h2>Ciao <?php echo $username; ?></h2>
+    
+</body>
+</html>
